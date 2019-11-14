369fee2f76c65346fefeb4317b208dad571a93fb
Hyakkimaru1 <49264569+Hyakkimaru1@users.noreply.github.com>
2019-11-14 17:58:45 +0700

    sửa long thành string của các thuộc tính date trong tour

---
+++ resources/js/product.js
@@ -0,0 +1,81 @@
+
+var imgsource = ['https://assets.catawiki.nl/assets/2019/5/22/e/f/8/ef80885e-c5c1-421a-b4c7-78b677059ca3.jpg','https://cdnmedia.baotintuc.vn/Upload/DMDnZyELa7xUDTdLsa19w/files/2019/10/thutuong/vietnam/joker.jpg'];
+$(document).ready(function(){
+    $('#infSeller').hide();
+    $('#description').click(function(){
+      $('#infSeller').hide();
+      $('#inf').show();
+      $('#seller').css( 'border','0');
+      $('#description').css('border', '2px solid #BFEEFA')
+    });
+    $('#seller').click(function(){
+      $('#inf').hide();
+      $('#seller').css('border', '2px solid #BFEEFA')
+      $('#infSeller').show();
+      $('#description').css( 'border','0');
+    });
+  });
+
+var i = 0;
+function changeImg(x){
+    if (x == document.getElementById("changimgR")){
+        $('#smallPic').children().eq(i).css('border','0')
+        i++;
+        if (i>(imgsource.length - 1))
+            i=0;
+        document.getElementById("mainPic").setAttribute("src",imgsource[i]);
+        $('#smallPic').children().eq(i).css('border', '2px solid #00AEEF');
+        
+    }
+    else {
+        $('#smallPic').children().eq(i).css('border','0')
+        i--;
+        if (i<0)
+            i=imgsource.length - 1;
+        document.getElementById("mainPic").setAttribute("src",imgsource[i]);   
+        $('#smallPic').children().eq(i).css('border', '2px solid #00AEEF');
+    }
+}
+
+
+function pic(x){
+    $('#smallPic').children().eq(i).css('border','0')
+    for (var j = 0;j<imgsource.length;j++)
+    {   
+        if(imgsource[j]==x.src)
+        {
+            i=j;
+        }
+    }
+    $('#smallPic').children().eq(i).css('border', '2px solid #00AEEF');
+    document.getElementById("mainPic").setAttribute("src",x.src);
+}
+
+var minBid = parseInt($('#minBid').text());
+$('#btPrimary').click( function(){
+    if ( parseInt($('#primary').val()) < minBid){ 
+           $( "#notiWrong" ).text( "Not valid!" ).show().fadeOut( 2000 );
+    }
+    else {
+        $('#currendBid').text($('#primary').val());
+    }
+});
+
+$('#btPrimaryAuto').click( function(){
+    if ( parseInt($('#primaryAuto').val()) < minBid)
+        $( "#notiWrongAuto" ).text( "Not valid!" ).show().fadeOut( 2000 );
+
+});
+
+
+
+$('#ViewAll').click(function(){
+    $('#top-overlay,#allBid').show();   
+    $().show();   
+});
+
+$('#top-overlay').click(function(){
+    $('#top-overlay,#allBid').hide();
+});
+
+
