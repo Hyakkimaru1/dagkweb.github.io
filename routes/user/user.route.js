a774cb166c168c6027cb94de5709387cb19cffbd
thkduy <56596480+thkduy@users.noreply.github.com>
2020-01-07 00:39:41 +0700

    fix lỗi

--- routes/user/user.route.js
+++ routes/user/user.route.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcryptjs');
 const userModel = require('../../models/user.model');
 const productModel = require('../../models/product.model');
 const moment = require('moment');
-const productModel = require('../../models/product.model');
 
 const router = express.Router();
 
